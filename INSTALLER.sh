9dbd5eef0a9dd545d3f77e9c0e462205ff3a402e
maxe-intern <92021832+maxe-intern@users.noreply.github.com>
2021-10-06 16:22:06 +0200

    Update INSTALLER.sh

--- INSTALLER.sh
+++ INSTALLER.sh
@@ -19,6 +19,8 @@ echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu\
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+echo "d903rh80h8ZRRZßZ§(=Z)UT=JGFHWGG)HJ$GH$)=H=N$§=)HJGIhjgioöjuilhohb"
+wait = 10
 #erstellt dependencie tree
 sudo apt-get update
 
